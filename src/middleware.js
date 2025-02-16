39f9b7d10f2250e574563d682ab87aec46a63bc1
Hikoyu <azrianpintar@gmail.com>
2025-02-16 21:51:32 +0700

    feat: initialize  middleware

---
+++ src/middleware.js
@@ -0,0 +1,30 @@
+import jsCookie from "js-cookie";
+import { NextResponse } from "next/server";
+const jwt = require('jsonwebtoken')
+
+const protectedRoutes = [
+    '/profile',
+    '/store',
+    '/trolley'
+]
+
+export default async function middleware(req) {
+    const path = req.nextUrl.pathname
+    const isPrivate = protectedRoutes.includes(path)
+
+    if (isPrivate) {
+        const token = req.cookies.get('token')?.value
+
+        // const decode = jwt.verify(token, process.env.SECRET)
+
+        if (!token) {
+            return NextResponse.redirect(new URL('/', req.url))
+        }
+    }
+
+    return NextResponse.next()
+}
+
+export const config = {
+    matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+}
