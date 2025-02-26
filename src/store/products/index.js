d2becb81bc1006c358126dad7e6250bfe1b3909d
Hikoyu <azrianpintar@gmail.com>
2025-02-26 12:53:52 +0700

    feat:  revamp state and dispacth

--- src/store/products/index.js
+++ src/store/products/index.js
@@ -6,8 +6,8 @@ export const getAll = createAsyncThunk('productSlice/getAll', async (params) => 
     return response.data
 })
 
-export const getOne = createAsyncThunk('productSlice/getOne', async () => {
-    const response = await Product.One()
+export const getOne = createAsyncThunk('productSlice/getOne', async (params) => {
+    const response = await Product.One(params)
     return response.data
 })
 
@@ -22,7 +22,22 @@ const initialState = {
     error: null,
     show: [],
     totalItems: 0,
-    data: []
+    data: {
+        isLoading: false,
+        isSuccess: false,
+        product: {
+            id: null,
+            name: '',
+            description: '',
+            price: 0,
+            stock: 0,
+            shop: {
+                id: null,
+                name: '',
+                address: ''
+            }
+        }
+    }
 }
 
 export const productSlice = createSlice({
@@ -48,14 +63,23 @@ export const productSlice = createSlice({
                 state.error = action.error.message
             })
             .addCase(getOne.pending, (state) => {
-                state.isLoading = true
+                state.data.isLoading = true
+                state.data.isSuccess = false
             })
             .addCase(getOne.fulfilled, (state, action) => {
-                state.isLoading = false,
-                state.data = action.payload.data
+                state.data.isLoading = false
+                state.data.isSuccess = true
+                state.data.product.id = action.payload.data.id
+                state.data.product.name = action.payload.data.name
+                state.data.product.description = action.payload.data.description
+                state.data.product.price = action.payload.data.price
+                state.data.product.stock = action.payload.data.stock
+                state.data.product.shop.id = action.payload.data.productToOwner.ownerToStore.id
+                state.data.product.shop.name = action.payload.data.productToOwner.ownerToStore.name
+                state.data.product.shop.address = action.payload.data.productToOwner.ownerToStore.address
             })
             .addCase(getOne.rejected, (state, action) => {
-                state.isLoading = false
+                state.data.isLoading = false
                 state.error = action.error.message
             })
             .addCase(findProduct.pending, (state) => {
