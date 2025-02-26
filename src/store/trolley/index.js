ea4788e959bfc89adad34c08b5a9309d473df414
Hikoyu <azrianpintar@gmail.com>
2025-02-26 14:06:19 +0700

    feat: new a trolley store

---
+++ src/store/trolley/index.js
@@ -0,0 +1,56 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { Trolley } from "@/services";
+
+export const insertItem = createAsyncThunk('trolleySlice/insertItem',  async (params) => {
+    const response = await Trolley.insert(params)
+    return response.data
+})
+
+export const getAllItemTrolley = createAsyncThunk('trolleySlice/getAllItemTrolley', async (params) => {
+    const response = await Trolley.list()
+    return response.data
+})
+
+const initialState = {
+    isLoading: false,
+    isSucces: false,
+    error: null,
+    data: []
+}
+
+export const trolleySlice = createSlice({
+    name: 'trolley',
+    initialState,
+    reducers: {
+
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(insertItem.pending, (state) => {
+                state.isLoading = true
+                state.isSucces = false
+            })
+            .addCase(insertItem.fulfilled, (state, action) => {
+                state.isLoading = false
+                state.isSucces = true
+            })
+            .addCase(insertItem.rejected, (state, action) => {
+                state.isLoading = false
+                state.error = action.error.message
+            })
+            .addCase(getAllItemTrolley.pending, (state) => {
+                state.isLoading = true
+                state.isSucces = false
+            })
+            .addCase(getAllItemTrolley.fulfilled, (state, action) => {
+                state.isLoading = true
+                state.isSucces = false
+                state.data = action.payload.data
+            }).addCase(getAllItemTrolley.rejected, (state, action) => {
+                state.isLoading = false
+                state.error = action.error.message
+            })
+    }
+})
+
+export default trolleySlice.reducer
