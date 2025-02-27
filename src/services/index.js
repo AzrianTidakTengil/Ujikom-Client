dc68ee1be7fea3bfe3a8e4ac8eac7b19799260f9
Hikoyu <azrianpintar@gmail.com>
2025-02-27 20:42:56 +0700

    feat: insert import transaction

--- src/services/index.js
+++ src/services/index.js
@@ -2,9 +2,11 @@ import * as Auth from './auth'
 import * as Users from './user'
 import * as Product from './product'
 import * as Trolley from './trolley'
+import * as Transaction from './transaction'
 export {
     Auth,
     Users,
     Product,
-    Trolley
+    Trolley,
+    Transaction
 }
