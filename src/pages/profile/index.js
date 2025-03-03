6d2db3de9e4db34749a6f066079e2ea96d7e48c0
Hikoyu <azrianpintar@gmail.com>
2025-03-03 14:55:08 +0700

    feat: new page

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,29 +1,100 @@
 import React, { Component } from "react";
-import { Container, Card, CardContent, Typography, Avatar, AppBar, Toolbar, Button } from "@mui/material";
+import { Container, Card, CardContent, Typography, Avatar, AppBar, Toolbar, Button, Box, Stack, Grid2 as Grid, Divider } from "@mui/material";
+import { connect } from "react-redux";
 
 class Profile extends Component {
   constructor(props) {
     super(props);
     this.state = {
       user: {
-        name: "John Doe",
-        email: "john.doe@example.com",
-        address: "123 Main St, Cityville",
-        avatar: "https://via.placeholder.com/150"
+        username: "",
+        fullname: "",
+        birthDay: "",
+        gender: "",
+        email: "",
+        telephone: "",
       }
     };
+  }
+
+  UNSAFE_componentWillReceiveProps() {
+    const {user} = this.props
+
+    if (user.isSuccess) {
+      this.setState({
+        user: {
+          ...this.state.user,
+          username: user.username,
+          fullname: `${user.firstname} ${user.lastname}`,
+          email: user.email,
+          telephone: user.telephone
+        }
+      })
+    }
   }
 
   render() {
     const { user } = this.state;
     return (
-      <Container maxWidth="md">
+      <Container>
         <Card sx={{ p: 3, textAlign: "center"}}>
           <CardContent>
-            <Avatar src={user.avatar} sx={{ width: 80, height: 80, margin: "auto", mt: 2 }} />
-            <Typography variant="h5" sx={{ mt: 2 }}>{user.name}</Typography>
-            <Typography variant="body2" color="textSecondary">{user.email}</Typography>
-            <Typography variant="body2" color="textSecondary">{user.address}</Typography>
+            <Avatar sx={{ width: 100, height: 100, margin: "auto", mt: 2 }} />
+            <Typography variant="h5" sx={{ marginY: 2 }}>{user.username}</Typography>
+            <Stack
+              direction="row"
+              divider={<Divider orientation="vertical" flexItem />}
+              spacing={2}       
+            >
+              <Box
+                width={'50%'}
+              >
+                <Grid container sx={{marginY: 2}}>
+                  <Grid size={4}>
+                    <Typography variant="body1" textAlign={'left'}>Nama</Typography>
+                  </Grid>
+                  <Grid size={8}>
+                    <Typography variant="body1" textAlign={'left'}>{user.fullname}</Typography>
+                  </Grid>
+                </Grid>
+                <Grid container sx={{marginY: 2}}>
+                  <Grid size={4}>
+                    <Typography variant="body1" textAlign={'left'}>Tanggal Lahir</Typography>
+                  </Grid>
+                  <Grid size={8}>
+                    <Typography variant="body1" textAlign={'left'}>{user.birthDay}</Typography>
+                  </Grid>
+                </Grid>
+                <Grid container sx={{marginY: 2}}>
+                  <Grid size={4}>
+                    <Typography variant="body1" textAlign={'left'}>Jenis Kelamin</Typography>
+                  </Grid>
+                  <Grid size={8}>
+                    <Typography variant="body1" textAlign={'left'}>{user.gender}</Typography>
+                  </Grid>
+                </Grid>
+              </Box>
+              <Box
+                width={'50%'}
+              >
+                <Grid container sx={{marginY: 2}}>
+                  <Grid size={4}>
+                    <Typography variant="body1" textAlign={'left'}>Email</Typography>
+                  </Grid>
+                  <Grid size={8}>
+                    <Typography variant="body1" textAlign={'left'}>{user.email}</Typography>
+                  </Grid>
+                </Grid>
+                <Grid container sx={{marginY: 2}}>
+                  <Grid size={4}>
+                    <Typography variant="body1" textAlign={'left'}>Nomor HP</Typography>
+                  </Grid>
+                  <Grid size={8}>
+                    <Typography variant="body1" textAlign={'left'}>{user.telephone}</Typography>
+                  </Grid>
+                </Grid>
+              </Box>
+            </Stack>
           </CardContent>
         </Card>
       </Container>
@@ -31,4 +102,20 @@ class Profile extends Component {
   }
 }
 
-export default Profile;
+const mapStateToProps = (state) => ({
+  user: {
+    isLoading: state.user.isLoading,
+    isSuccess: state.user.isSuccess,
+    username: state.user.user.username,
+    firstname: state.user.user.firstname,
+    lastname: state.user.user.lastname,
+    email: state.user.user.email,
+    telephone: state.user.user.telephone
+  },
+})
+
+const mapDispatchToProps = {
+
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Profile);
