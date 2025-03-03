88045816651afeb779e1c197440745c848e75391
Hikoyu <azrianpintar@gmail.com>
2025-03-03 20:57:23 +0700

    feat: component a tab

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-import { Container, Card, CardContent, Typography, Avatar, AppBar, Toolbar, Button, Box, Stack, Grid2 as Grid, Divider } from "@mui/material";
+import { Container, Card, CardContent, Typography, Avatar, AppBar, Toolbar, Button, Box, Stack, Grid2 as Grid, Divider, Tab, Tabs, InputAdornment, TextField } from "@mui/material";
 import { connect } from "react-redux";
+import { SearchOutlined } from "@mui/icons-material";
 
 class Profile extends Component {
   constructor(props) {
@@ -13,7 +14,8 @@ class Profile extends Component {
         gender: "",
         email: "",
         telephone: "",
-      }
+      },
+      renderTabs: 'address'
     };
   }
 
@@ -33,14 +35,65 @@ class Profile extends Component {
     }
   }
 
+  renderMoreAddress = () => {
+    return (
+      <Box
+        sx={{
+          paddingY: 2
+        }}
+      >
+        <Grid container spacing={2}>
+          <Grid size={10}>
+            <TextField
+              fullWidth
+              size="small"
+              hiddenLabel
+              slotProps={{ input: {
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <SearchOutlined/>
+                  </InputAdornment>
+                )
+              }}}
+            />
+          </Grid>
+          <Grid size={2}>
+            <Button variant="contained" fullWidth>
+              Cari
+            </Button>
+          </Grid>
+        </Grid>
+        <Box sx={{
+          minHeight: '30rem'
+        }}>
+
+        </Box>
+      </Box>
+    )
+  }
+
+  renderTransaction = () => {
+    return (
+      <Box>
+
+      </Box>
+    )
+  }
+
+  handleChangeValueTab = (event, newValue) => {
+    this.setState({
+      renderTabs: newValue
+    })
+  }
+
   render() {
-    const { user } = this.state;
+    const { user, renderTabs } = this.state;
     return (
       <Container>
         <Card sx={{ p: 3, textAlign: "center"}}>
           <CardContent>
             <Avatar sx={{ width: 100, height: 100, margin: "auto", mt: 2 }} />
-            <Typography variant="h5" sx={{ marginY: 2 }}>{user.username}</Typography>
+            <Typography variant="h5" sx={{ marginY: 2 }} fontWeight={600}>{user.username}</Typography>
             <Stack
               direction="row"
               divider={<Divider orientation="vertical" flexItem />}
@@ -96,6 +149,24 @@ class Profile extends Component {
               </Box>
             </Stack>
           </CardContent>
+        </Card>
+        <Card sx={{ p: 3, textAlign: "center", marginTop: 4}}>
+          <Box>
+            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+              <Tabs
+                onChange={this.handleChangeValueTab}
+                value={renderTabs}
+              >
+                <Tab value="address" label="Detail Alamat"/>
+                <Tab value="transaction" label="Riwayat Transaksi"/>
+              </Tabs>
+            </Box>
+            {
+              renderTabs === 'address' ? this.renderMoreAddress() :
+              renderTabs === 'transaction' ? this.renderTransaction :
+              ''
+            }
+          </Box>
         </Card>
       </Container>
     );
