de9c07d019e72963d1aa3c71022109c8bc5c172f
Hikoyu <azrianpintar@gmail.com>
2025-03-05 16:47:14 +0700

    feat: new tab an address

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
-import { Container, Card, CardContent, Typography, Avatar, AppBar, Toolbar, Button, Box, Stack, Grid2 as Grid, Divider, Tab, Tabs, InputAdornment, TextField } from "@mui/material";
+import { Container, Card, CardContent, Typography, Avatar, AppBar, Toolbar, Button, Box, Stack, Grid2 as Grid, Divider, Tab, Tabs, InputAdornment, TextField, Paper, Chip, CircularProgress } from "@mui/material";
 import { connect } from "react-redux";
 import { SearchOutlined } from "@mui/icons-material";
+import { getAll as getAllAddress, find } from "@/store/address";
 
 class Profile extends Component {
   constructor(props) {
@@ -15,12 +16,17 @@ class Profile extends Component {
         email: "",
         telephone: "",
       },
-      renderTabs: 'address'
+      renderTabs: 'address',
+      addresses: []
     };
   }
 
+  UNSAFE_componentWillMount() {
+    this.props.getAllAddress()
+  }
+
   UNSAFE_componentWillReceiveProps() {
-    const {user} = this.props
+    const {user, address} = this.props
 
     if (user.isSuccess) {
       this.setState({
@@ -33,9 +39,18 @@ class Profile extends Component {
         }
       })
     }
+
+    if (address.isSuccess) {
+      this.setState({
+        addresses: address.list.data
+      })
+    }
   }
 
   renderMoreAddress = () => {
+    const {addresses} = this.state
+    const {address} = this.props
+
     return (
       <Box
         sx={{
@@ -55,21 +70,99 @@ class Profile extends Component {
                   </InputAdornment>
                 )
               }}}
+              onChange={this.handleChangeSearchAddress}
             />
           </Grid>
-          <Grid size={2}>
+          <Grid 
+            size={2}
+            sx={{
+              display: 'flex',
+              alignItems: 'center'
+            }}
+          >
             <Button variant="contained" fullWidth>
-              Cari
+              Tambah Alamat
             </Button>
           </Grid>
         </Grid>
         <Box sx={{
           minHeight: '30rem'
         }}>
-
+          {
+            address.isLoading ? (
+              <CircularProgress sx={{marginTop: 2}}/>
+            ) : addresses.map((val) => (
+              <Paper
+                key={val.id}
+                sx={{
+                  marginY: 2,
+                  p: 2
+                }}
+              >
+                <Grid container>
+                  <Grid size={10}>
+                    <Stack
+                      sx={{marginY: 2}}
+                      direction="row"
+                      alignItems={'center'}
+                      divider={<Divider orientation="vertical" flexItem />}
+                      spacing={2} 
+                    >
+                      <Typography variant="body1" fontWeight={600}>{val.receiver}</Typography>
+                      <Typography variant="body1">{val.name}</Typography>
+                      {
+                        val.selectedAddressUser ? (
+                          <Chip label="Dipilih" color="success"/>
+                        ) : ''
+                      }
+                      {
+                        val.defaultAddressUser ? (
+                          <Chip label="Utama" />
+                        ) : ''
+                      }
+                    </Stack>
+                    <Typography variant="body1" sx={{marginY: 2}} textAlign={'left'}>{val.telephone}</Typography>
+                    <Typography variant="body1" sx={{marginY: 2}} textAlign={'left'}>{`${val.address}, ${val.district}, ${val.city}, ${val.province}, ${val.postal_code} ${val.notes ? `(${val.notes})` : ''}`}</Typography>
+                  </Grid>
+                  <Grid 
+                    size={2}
+                    sx={{
+                      display: 'flex',
+                      justifyContent: 'center',
+                      flexDirection: 'column'
+                    }}
+                  >
+                    {
+                      !val.selectedAddressUser ? (
+                        <Button variant="contained" color="success">
+                          Pilih
+                        </Button>
+                      ) : ''
+                    }
+                    {
+                      !val.defaultAddressUser ? (
+                        <Button variant="outlined" color="success" sx={{marginTop: 2}}>
+                          Jadikan Utama
+                        </Button>
+                      ) : ''
+                    }
+                    <Button variant="outlined" color="success" sx={{marginTop: 2}}>
+                      Edit
+                    </Button>
+                  </Grid>
+                </Grid>
+              </Paper>
+            ))
+          }
         </Box>
       </Box>
     )
+  }
+
+  handleChangeSearchAddress = (event) => {
+    const {value} = event.target
+
+    this.props.find({name: value})
   }
 
   renderTransaction = () => {
@@ -88,6 +181,7 @@ class Profile extends Component {
 
   render() {
     const { user, renderTabs } = this.state;
+    const {address} = this.props
     return (
       <Container>
         <Card sx={{ p: 3, textAlign: "center"}}>
@@ -183,10 +277,18 @@ const mapStateToProps = (state) => ({
     email: state.user.user.email,
     telephone: state.user.user.telephone
   },
+  address: {
+    isLoading: state.address.isLoading,
+    isSuccess: state.address.isSuccess,
+    list: state.address.list,
+    address: state.address.address,
+    error: state.address.list,
+  }
 })
 
 const mapDispatchToProps = {
-
+  getAllAddress,
+  find
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Profile);
