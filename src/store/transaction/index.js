44fb5138378471251383ea5ffdd4fcc483f3c08c
Hikoyu <azrianpintar@gmail.com>
2025-03-06 21:00:30 +0700

    feat: new store

--- src/store/transaction/index.js
+++ src/store/transaction/index.js
@@ -11,10 +11,21 @@ export const getTransaction = createAsyncThunk('transactionSlice/getTransaction'
     return response.data
 })
 
+export const getAllTransaction = createAsyncThunk('transactionSlice/getAllTransaction', async (params) => {
+    const response = await Transaction.getAll(params)
+    return response.data
+})
+
+export const findTransaction = createAsyncThunk('transaction/findTransaction', async (params) => {
+    const response = await Transaction.find(params)
+    return response.data
+})
+
 const initialState = {
     isLoading: false,
     isSuccess: false,
     error: null,
+    list: [],
     data: {}
 }
 
@@ -49,6 +60,32 @@ export const transactionSlice = createSlice({
                 state.data = action.payload
             })
             .addCase(getTransaction.rejected, (state, action) => {
+                state.isLoading = true
+                state.error = action.error.message
+            })
+            .addCase(getAllTransaction.pending, (state) => {
+                state.isLoading = true
+                state.isSuccess = false
+            })
+            .addCase(getAllTransaction.fulfilled, (state, action) => {
+                state.list = action.payload
+                state.isSuccess = true
+                state.isLoading = false
+            })
+            .addCase(getAllTransaction.rejected, (state, action) => {
+                state.isLoading = true
+                state.error = action.error.message
+            })
+            .addCase(findTransaction.pending, (state) => {
+                state.isLoading = true
+                state.isSuccess = false
+            })
+            .addCase(findTransaction.fulfilled, (state, action) => {
+                state.list = action.payload
+                state.isSuccess = true
+                state.isLoading = false
+            })
+            .addCase(findTransaction.rejected, (state, action) => {
                 state.isLoading = true
                 state.error = action.error.message
             })
