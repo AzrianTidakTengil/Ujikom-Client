4fd95a31baaacacce3a31a03d94a7551bbb76513
Hikoyu <azrianpintar@gmail.com>
2025-03-17 12:57:26 +0700

    feat: new a service for role seller

--- src/services/shop.js
+++ src/services/shop.js
@@ -5,3 +5,46 @@ export const BySeller = () => {
         endpoints.STORE.SELLER
     )
 }
+
+export const UpdateShop = (params) => {
+    return apiServiceV1.put(
+        endpoints.STORE.UPDATE,
+        params
+    )
+}
+
+export const Balance = () => {
+    return apiServiceV1.get(
+        endpoints.STORE.BALANCE
+    )
+}
+
+export const InTrolley = () => {
+    return apiServiceV1.get(
+        endpoints.STORE.INTROLLEY
+    )
+}
+
+export const Order = () => {
+    return apiServiceV1.get(
+        endpoints.STORE.ORDER
+    )
+}
+
+export const Operaion = () => {
+    return apiServiceV1.get(
+        endpoints.STORE.OPERATION
+    )
+}
+
+export const ProductShop = () => {
+    return apiServiceV1.get(
+        endpoints.PRODUCTS.STORE
+    )
+}
+
+export const PopularAnalysis = () => {
+    return apiServiceV1.get(
+        endpoints.PRODUCTS.POPULAR
+    )
+}
