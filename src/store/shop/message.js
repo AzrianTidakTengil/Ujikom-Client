265e092db1d9e84247ce32a1dfce9cbe5a17eb25
Hikoyu <azrianpintar@gmail.com>
2025-03-22 15:30:56 +0700

    feat: new idea for condition when success async and added shop address

---
+++ src/store/shop/message.js
@@ -0,0 +1,21 @@
+export default {
+    STORE: {
+        SELLER: 'SellerShop',
+        CREATE: 'CreateShop',
+        UPDATE: 'UpdateShop',
+        LABEL: {
+            GET: 'GetLabelShop',
+            CREATE: 'CreateLabelShop',
+            UPDATE: 'UpdateLabelShop',
+        },
+        BALANCE: 'BalanceShop',
+        INTROLLEY: 'InTrolleyShop',
+        ORDER: 'OrderShop',
+        HANDLEORDER: 'HandleOrderShop',
+        OPERATION: 'GetOperationShop',
+        ADDRESS: {
+            GET: 'GetAddressShop',
+            CRETEORUPDATE: 'CreateOrUpdateAddressShop'
+        },
+    },
+}
