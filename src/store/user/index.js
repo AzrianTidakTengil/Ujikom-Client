10736cf06b8223117b04cee37e31f3971fd4ce89
Hikoyu <azrianpintar@gmail.com>
2025-03-29 02:17:37 +0700

    feat: new action update avatar user

--- src/store/user/index.js
+++ src/store/user/index.js
@@ -1,9 +1,15 @@
 import { Users } from "@/services"
+import { CreateOrUpadateAvatar } from "@/services/user"
 import { fabClasses } from "@mui/material"
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
 
 export const getUser = createAsyncThunk('userSlice/get', async (params) => {
     const response = await Users.get()
+    return response.data
+})
+
+export const updateAvatarUser = createAsyncThunk('userSlice/UpdateAvatarUser', async (params) => {
+    const response = await CreateOrUpadateAvatar(params)
     return response.data
 })
 
@@ -41,6 +47,18 @@ export const userSlice = createSlice({
         }).addCase(getUser.rejected, (state,action) => {
             state.isLoading = false
             state.error = action.error.message
+            state.isSuccess = false
+        })
+        .addCase(updateAvatarUser.pending, (state) => {
+            state.isLoading = true
+            state.isSuccess = false
+        })
+        .addCase(updateAvatarUser.fulfilled, (state, action) => {
+            state.isLoading = false
+            state.isSuccess = true
+        })
+        .addCase(updateAvatarUser.fulfilled, (state, action) => {
+            state.isLoading = false
             state.isSuccess = false
         })
     }
