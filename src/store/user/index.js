2998c85500eac6e1353cafd93d4d570e12272fed
Hikoyu <azrianpintar@gmail.com>
2025-03-29 02:22:51 +0700

    feat: configuration with system message step by step

--- src/store/user/index.js
+++ src/store/user/index.js
@@ -2,6 +2,7 @@ import { Users } from "@/services"
 import { CreateOrUpadateAvatar } from "@/services/user"
 import { fabClasses } from "@mui/material"
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
+import UserMessage from './message'
 
 export const getUser = createAsyncThunk('userSlice/get', async (params) => {
     const response = await Users.get()
@@ -23,7 +24,8 @@ const initialState = {
         lastname: '',
         email: '',
         telephone: ''
-    }
+    },
+    message: '',
 }
 
 export const userSlice = createSlice({
@@ -34,9 +36,12 @@ export const userSlice = createSlice({
     },
     extraReducers: (builder) => {
         builder.addCase(getUser.pending, (state) => {
+            state.message = ''
+            state.isSuccess = false
             state.isLoading = true
         }).addCase(getUser.fulfilled, (state, action) => {
             const {username, firstname, lastname, email, telephone} = action.payload.data
+            state.message = UserMessage.USER.GET
             state.isLoading = false
             state.user.email = email
             state.user.firstname = firstname
@@ -45,19 +50,23 @@ export const userSlice = createSlice({
             state.user.username = username
             state.isSuccess = true
         }).addCase(getUser.rejected, (state,action) => {
+            state.message = UserMessage.USER.GET
             state.isLoading = false
             state.error = action.error.message
             state.isSuccess = false
         })
         .addCase(updateAvatarUser.pending, (state) => {
+            state.message = ''
             state.isLoading = true
             state.isSuccess = false
         })
         .addCase(updateAvatarUser.fulfilled, (state, action) => {
+            state.message = UserMessage.USER.AVATAR
             state.isLoading = false
             state.isSuccess = true
         })
         .addCase(updateAvatarUser.fulfilled, (state, action) => {
+            state.message = UserMessage.USER.AVATAR
             state.isLoading = false
             state.isSuccess = false
         })
