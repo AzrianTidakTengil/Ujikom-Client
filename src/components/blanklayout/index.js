6a56ddcc260bc019200c6423ef373dcb9d2b13a4
Hikoyu <azrianpintar@gmail.com>
2025-04-09 20:20:43 +0700

    feat: create layout for page register

---
+++ src/components/blanklayout/index.js
@@ -0,0 +1,33 @@
+import Navbar from "../nabar";
+import Footer from "../footer";
+import Link from "next/link";
+import { AppBar, Toolbar, Typography, Box } from "@mui/material";
+
+export default function BlankLayout({children}) {
+    return (
+        <>
+            <AppBar position="fixed">
+                <Toolbar sx={{ justifyContent: "space-between" }}>
+                    <Link href="/" style={{textDecoration: 'none', color: '#fff'}}><Typography variant='h4' fontSize={'1.25rem'} fontWeight={800}>Popping</Typography></Link>
+                </Toolbar>
+            </AppBar>
+            <main style={{
+                marginTop: '4.5rem',
+                padding: '1rem 0.5rem'
+            }}>
+                <Box
+                    sx={{
+                        minHeight: '60vh'
+                    }}
+                >
+                    {children}
+                </Box>
+            </main>
+            <div style={{
+                width: '100%'
+            }}>
+                <Footer></Footer>
+            </div>
+        </>
+    )
+}
