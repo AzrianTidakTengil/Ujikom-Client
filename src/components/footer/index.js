f55fbf94d6d330bd869bcfdda405c9b8577185fa
Hikoyu <azrianpintar@gmail.com>
2025-06-16 05:00:51 +0700

    feat(component/footer): revamp style footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,53 +1,111 @@
 const { Component } = require("react");
-import { Container, Typography, Box, IconButton, Grid2 as Grid, Divider } from "@mui/material";
+import {
+  Container,
+  Typography,
+  Box,
+  IconButton,
+  Grid2 as Grid,
+  Divider,
+  createTheme,
+  ThemeProvider,
+} from "@mui/material";
 import InstagramIcon from "@mui/icons-material/Instagram";
+import { palleteV1 } from "@/assets/css/template";
+import Link from "next/link";
+import { Facebook, Twitter } from "@mui/icons-material";
 
-class Footer extends Component{
-    constructor(props) {
-        super()
-        this.state ={
+const theme = createTheme({
+  ...palleteV1,
+});
 
-        }
-    }
+class Footer extends Component {
+  constructor(props) {
+    super();
+    this.state = {};
+  }
 
-    render() {
-        return(
-            <Box component="footer" sx={{ bgcolor: "#333", color: "white", py: 3, mt: 5,  width: "100%" }}>
-            <Container maxWidth="xl">
-                <Grid container spacing={4} justifyContent="space-around">
-                <Grid xs={12} sm={4}>
-                    <Typography variant="h6">Help</Typography>
-                    <Typography variant="body2">Customer Support</Typography>
-                    <Typography variant="body2">FAQs</Typography>
-                    <Typography variant="body2">Contact Us</Typography>
-                </Grid>
-                <Grid xs={12} sm={4}>
-                    <Typography variant="h6">About the Company</Typography>
-                    <Typography variant="body2">Our Story</Typography>
-                    <Typography variant="body2">Careers</Typography>
-                    <Typography variant="body2">Press</Typography>
-                </Grid>
-                <Grid xs={12} sm={4}>
-                    <Typography variant="h6">Follow Us</Typography>
+  render() {
+    return (
+      <ThemeProvider theme={theme}>
+        <Box
+          sx={{
+            bgcolor: theme.palette.secondary.main,
+            color: theme.palette.secondary.contrastText,
+          }}
+          className="w-full p-6"
+        >
+          <div className="grid grid-cols-3 gap-4">
+            <div className="px-4">
+              <h5 className="text-4xl font-semibold mb-4">Help</h5>
+              <Link href={"/"}>
+                <p className="text-md font-light">Customer Support</p>
+              </Link>
+              <Link href={"/"}>
+                <p className="text-md font-light">FAQs</p>
+              </Link>
+              <Link href={"/"}>
+                <p className="text-md font-light">Contact Us</p>
+              </Link>
+            </div>
+            <div className="px-4">
+              <h5 className="text-4xl font-semibold mb-4">About the Company</h5>
+              <Link href={"/"}>
+                <p className="text-md font-light">Our Story</p>
+              </Link>
+              <Link href={"/"}>
+                <p className="text-md font-light">Careers</p>
+              </Link>
+              <Link href={"/"}>
+                <p className="text-md font-light">Press</p>
+              </Link>
+            </div>
+            <div className="px-4">
+              <h5 className="text-4xl font-semibold mb-4">Follow Us</h5>
+              <div className="flex space-x-2">
+                <Link href={"/"}>
+                  <p className="text-md font-light">
                     <IconButton
-                    href="https://www.instagram.com/yourprofile"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    sx={{ color: "white" }}
+                      href="https://www.instagram.com/"
+                      target="_blank"
+                      rel="noopener noreferrer"
                     >
-                        <InstagramIcon fontSize="large" />
+                      <InstagramIcon fontSize="large" />
                     </IconButton>
-                    <Typography variant="body2" sx={{ mt: 1 }}>Stay connected with us on social media</Typography>
-                </Grid>
-                </Grid>
-                <Divider sx={{ bgcolor: "white", my: 2 }} />
-                <Typography variant="body2" sx={{ mt: 2, textAlign: "center" }}>
-                    &copy; Azrian Muhammadin Hanif {new Date().getFullYear()}
-                </Typography>
-            </Container>
-            </Box>
-        )
-    }
+                  </p>
+                </Link>
+                <Link href={"/"}>
+                  <p className="text-md font-light">
+                    <IconButton
+                      href="https://www.facebook.com/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <Facebook fontSize="large" />
+                    </IconButton>
+                  </p>
+                </Link>
+                <Link href={"/"}>
+                  <p className="text-md font-light">
+                    <IconButton
+                      href="https://www.facebook.com/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <Twitter fontSize="large" />
+                    </IconButton>
+                  </p>
+                </Link>
+              </div>
+            </div>
+          </div>
+          <hr className="my-4" />
+          <p className="text-center">
+            &copy; Azrian Muhammadin Hanif {new Date().getFullYear()}
+          </p>
+        </Box>
+      </ThemeProvider>
+    );
+  }
 }
 
-export default Footer
+export default Footer;
