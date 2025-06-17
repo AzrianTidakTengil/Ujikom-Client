51c1646e7df22998dc8000f717dce45a47f466a7
Hikoyu <azrianpintar@gmail.com>
2025-06-17 17:27:29 +0700

    fix(layout/default): error does not render in client

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,24 +1,18 @@
 import { useBreakpoint } from "@/config";
 import { NavbarMobile, Navbar, Footer } from "..";
+import { useEffect, useState } from "react";
+import { useMediaQuery, useTheme } from "@mui/material";
 
-export default function Layout({children}) {
-    const breakpoint = useBreakpoint()
-
-    return (
-        <>
-            {
-                breakpoint === 'default' ? (
-                    <NavbarMobile></NavbarMobile>
-                ) : (
-                    <Navbar></Navbar>
-                )
-            }
-            <main className="px-2 py-4 mt-24">
-                {children}
-            </main>
-            <div className="w-full">
-                <Footer></Footer>
-            </div>
-        </>
-    )
+export default function Layout({ children }) {
+  const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
+  return (
+    <>
+      {isSmallScreen ? <NavbarMobile /> : <Navbar />}
+      <main className="px-2 py-4 mt-24">{children}</main>
+      <div className="w-full">
+        <Footer></Footer>
+      </div>
+    </>
+  );
 }
